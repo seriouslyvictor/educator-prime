c285a78784c2ff34c644d7472309bc3da77625fc
seriouslyvictor <schiavovictor@outlook.com>
2026-06-12 00:27:46 -0300

    Add shadcn admin UI foundation

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles/tokens.css";
 import "./styles/base.css";
+import "./styles/tailwind.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
